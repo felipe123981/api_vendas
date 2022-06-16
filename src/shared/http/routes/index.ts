27cfd649661e5542c96b163b8a7116025ce730c5
felipe123981 <filipe.timachine@gmail.com>
2022-06-16 10:00:14 -0300

    first commit

---
+++ src/shared/http/routes/index.ts
@@ -0,0 +1,15 @@
+import { Router } from 'express';
+import productsRouter from '@modules/products/routes/products.routes';
+import usersRouter from '@modules/users/routes/users.routes';
+
+const routes = Router();
+
+routes.use('/products', productsRouter);
+
+routes.use('/users', usersRouter);
+
+routes.get('/', (request, response) => {
+  return response.json({ message: 'Hello Dev!' });
+});
+
+export default routes;
