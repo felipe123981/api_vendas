d93dd889d964daa96c7c7c7bd51fa4fe8827d0c0
felipe123981 <filipe.timachine@gmail.com>
2022-06-24 12:45:53 -0300

    customers

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -4,6 +4,7 @@ import usersRouter from '@modules/users/routes/users.routes';
 import sessionsRouter from '@modules/users/routes/sessions.routes';
 import passwordRouter from '@modules/users/routes/password.routes';
 import profileRouter from '@modules/users/routes/profile.routes';
+import customersRouter from '@modules/customers/routes/customers.routes';
 
 const routes = Router();
 
@@ -11,7 +12,8 @@ routes.use('/products', productsRouter);
 routes.use('/users', usersRouter);
 routes.use('/sessions', sessionsRouter);
 routes.use('/password', passwordRouter);
-routes.use('profile',profileRouter);
+routes.use('/profile',profileRouter);
+routes.use('/customers', customersRouter);
 
 routes.get('/', (request, response) => {
   return response.json({ message: 'Hello Dev!' });
