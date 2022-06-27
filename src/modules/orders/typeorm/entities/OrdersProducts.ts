def32ba0973698b427a5c79a95a85eaa3649a5e2
felipe123981 <filipe.timachine@gmail.com>
2022-06-26 23:47:26 -0300

    Orders

--- src/modules/orders/typeorm/entities/OrdersProducts.ts
+++ src/modules/orders/typeorm/entities/OrdersProducts.ts
@@ -16,17 +16,19 @@ class OrdersProducts {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
-  @ManyToOne(() => Customer)
-  @JoinColumn({ name: 'customer_id' })
-  customer: Customer;
-
   @ManyToOne(() => Order, order => order.order_products)
   @JoinColumn({ name: 'order_id' })
   order: Order;
 
   @ManyToOne(() => Product, product => product.order_products)
-  @JoinColumn({ name: 'order_id' })
+  @JoinColumn({ name: 'product_id' })
   product: Product;
+
+  @Column()
+  order_id: string;
+
+  @Column()
+  product_id: string;
 
   @Column('decimal')
   price: number;
