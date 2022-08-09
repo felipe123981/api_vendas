5623aacd52b39c5f3d269d2018ac06f8619f49d3
felipe123981 <filipe.timachine@gmail.com>
2022-08-09 09:14:01 -0300

    first commit

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -9,11 +9,14 @@ import '@shared/typeorm';
 import { errors } from 'celebrate';
 import uploadConfig from '@config/upload';
 import { pagination } from 'typeorm-pagination';
+import rateLimiter from './middlewares/rateLimiter';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+
+app.use(rateLimiter);
 
 app.use(pagination);
 
